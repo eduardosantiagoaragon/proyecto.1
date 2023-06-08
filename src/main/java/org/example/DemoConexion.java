051fb3d3f9720e0ddd521a12bf22f0f7fa3bdfc1
LUIS EDUARDO GONZALEZ SANTIAGO <lalinrequegonzalezgmail.conm>
2023-06-07 19:43:07 -0600

    .17

--- src/main/java/org/example/DemoConexion.java
+++ src/main/java/org/example/DemoConexion.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.sql.*;
+
+import org.sqlite.JDBC;
+
 public class DemoConexion {
     public static void main(String[] args) {
+        String db_url = "jdbc:sqlite:JDBCDE.db";
+        Connection conexion = null;
 
+        try{
+            Class.forName("org.sqlite.JDBC");
+            conexion = DriverManager.getConnection(db_url);
+            Statement stm = conexion.createStatement();
+            ResultSet rst = stm.executeQuery("SELECT * FROM DEPORTES EXTREMOS;");
+            while (rst.next()){
+                System.out.println(rst.getString(1) + "" + rst.getString(2));
+            }
+            conexion.close();
+        }catch (ClassNotFoundException cne){
+            cne.printStackTrace();
+        }catch (SQLException sqle){
+            sqle.printStackTrace();
+        }
     }
 }
